9cc19cc8d1c57c23e85ffb7983020341635b10d0
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2022-10-02 20:12:58 +0000

    SC - update app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from T3C11 Sunday Group Website :D
         </p>
         <a
           className="App-link"
